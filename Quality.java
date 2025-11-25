889b652a89c10e7efd7243a7e435a9f0578e365b
Sean Lowry <sel810213@csu.fullerton.edu>
2025-11-24 18:57:22 -0800

    added Quality.java

---
+++ Quality.java
@@ -0,0 +1,6 @@
+public enum Quality {
+    AGAIN,      // user failed recall
+    HARD,       // recalled with difficulty
+    GOOD,       // recalled correctly
+    EASY        // recalled effortlessly
+}
